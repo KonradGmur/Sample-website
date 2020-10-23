a25051e65a33c10a7d7d8f46f3096828f57bfb43
Konrad <konrad.gmur@gmail.com>
2020-10-23 18:28:09 +0200

    Add modal support in index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,15 @@
-import 'bootstrap' 
-import '../scss/main.scss'
+import "bootstrap";
+import "../scss/main.scss";
+
+const modal = document.querySelector(".my-modal");
+const openBtn = document.querySelector(".open-modal");
+const closeBtn = document.querySelector(".close-modal-btn");
+const message = document.querySelector(".video");
+
+openBtn.addEventListener("click", () => {
+  modal.showModal();
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.close();
+});
